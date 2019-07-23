200029fbbb7612499361a8fa653d75afc0ea2a40
Soro Cat <sorousha326@gmail.com>
2019-07-23 14:58:16 -0700

    add changes in js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-var crypto = function(sentence){
+var crypto = function(){
+  var square = "The quick brown fox jumped over the lazy dog"
+  console.log(square);
+  var crypt = []
+}
 
 // user interface logic
 $(document).ready(function() {
   $("form#crypto").submit(function(event) {
     event.preventDefault();
     var words = $('#words').val()
+    var result = crypto()
   })
  });
