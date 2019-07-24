c73412dc369b37179a818ccede6f1700dbb13bb7
Soro Cat <sorousha326@gmail.com>
2019-07-24 14:13:37 -0700

    add changes in js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 var cryptos = function(){
   var square = "Thequickbrownfoxjumpedoverthelazydog"
   console.log(square);
-  var math;
-  var len = math.ceil(math.sqrt)(words.length);
-  var letters = [["t,h,e,q,u,i"],["c,k,b,r,o,w"]["n,f,o,x,j,u"]["m,p,e,d,o,v"]["e,r,t,h,e,l"]["a,z,y,d,o,g"]]
+  var len = Math.ceil(Math.sqrt(words.length));
+  var letters = square.split("")
+  for (x=0; x < square.length; x+=len) {
+    console.log(x)
+    var topush = letters.slice(x,x+len)
+    console.log(topush)
+  }
   var crypt = []
 }
 
