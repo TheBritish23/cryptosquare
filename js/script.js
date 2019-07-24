540f20d29284bb5afedc504091f62b7fada2d605
Soro Cat <sorousha326@gmail.com>
2019-07-24 14:53:16 -0700

    add changes in js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 var cryptos = function(){
+  var grid = []
   var square = "Thequickbrownfoxjumpedoverthelazydog"
   console.log(square);
-  var len = Math.ceil(Math.sqrt(words.length));
+  var len = Math.ceil(Math.sqrt(square.length));
   var letters = square.split("")
   for (x=0; x < square.length; x+=len) {
     console.log(x)
     var topush = letters.slice(x,x+len)
-    console.log(topush)
+    grid.push(topush)
   }
+  console.log(grid)
   var crypt = []
 }
 
 // user interface logic
 $(document).ready(function() {
-  $("form#crypto").submit(function(event) {
+  $("form#cryptos").submit(function(event) {
     event.preventDefault();
     var words = $('#words').val()
     var result = cryptos()
